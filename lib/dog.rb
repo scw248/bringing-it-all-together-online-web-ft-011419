9e1d391b457f4c46e067db3ba8ef0f44dc28a4b1
Stephen Wright <scw248@gmail.com>
2019-02-15 16:03:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -70,9 +70,9 @@ class Dog
   end 
   
   def self.new_from_db(row)
-    new_dog.id = row[0]
-    new_dog.name =  row[1]
-    new_dog.breed = row[2]
+    id = row[0]
+    name =  row[1]
+    breed = row[2]
     new_dog = self.new(row)
     new_dog 
   end
